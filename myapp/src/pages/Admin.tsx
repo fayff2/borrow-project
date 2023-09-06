e7530d1d8a0443efe9d860a2ee530ccd9ddfee40
Bandar1014 <Bandar1014@gmail.com>
2023-09-06 20:22:57 +0300

    get and update user info

--- myapp/src/pages/Admin.tsx
+++ myapp/src/pages/Admin.tsx
@@ -1,7 +1,54 @@
+import axios from "axios"
+import React from "react"
 
+type Ishow ={
+  id:string;
+  NId:string;
+  email:string;
+  name:string;
+  password:string;
+  phone:string;
+  status:boolean;
+}
 
 export default function Admin() {
+  const [showUser , setShowUser]= React.useState<Ishow[]>([])
+
+  const id = localStorage.getItem("isLogin")
+
+  React.useEffect(()=>{
+    axios.get(`https://64f753f19d775408495375aa.mockapi.io/useraccount/`).then((res)=>{
+      console.log(res.data)
+      setShowUser(res.data)
+    })
+  },[])
+
+function updateStatus() {
+  axios.put(`https://64f753f19d775408495375aa.mockapi.io/useraccount/${id}`, {
+    status:true
+  })
+  .then((res) => {
+    console.log(res.data.status)
+    setShowUser(res.data)
+  })
+}
+
+
   return (
-    <div>Admin</div>
+    <>
+    {showUser.map((item)=> (
+      
+      <>
+      <p>{showUser.name}</p>
+      <p>{showUser.id}</p>
+      <p>{showUser.NID}</p>
+      <p>{showUser.email}</p>
+      <p>{showUser.phone}</p>
+      <p>{showUser.status ? "مفعل" : "غير مفعل"}</p>
+      <button onClick={updateStatus}>تفعيل</button>
+      </>
+    ))}
+      
+    </>
   )
 }
