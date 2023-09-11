c5260803c02f530bdaaa6de13ef6799aa753d7a7
Bandar1014 <Bandar1014@gmail.com>
2023-09-11 13:29:57 +0300

    pay

--- myapp/src/pages/pay.tsx
+++ myapp/src/pages/pay.tsx
@@ -29,11 +29,45 @@ export default function pay() {
 
   return (
     <>
-         <input type="text" placeholder="ادخل الاسم الثلاثي" className="border" onChange={e=>setDataBorrow({...dataBorrow, name:(e.target.value)})}/><br/>
-        <input type="text"  placeholder="وصف العملية " className="border" onChange={e=>setDataBorrow({...dataBorrow, borrowDetails:(e.target.value)})}/><br/>
+    <div className='flex flex-col gap-10'>
+        <div className=''>
+            <div className=' border h w-auto flex flex-col  '>
+        <label htmlFor="">إصدار فاتورة  </label>
+                <div className='flex gap-4'>
+                 <input type="text" placeholder="ادخل الاسم الثلاثي" className="border" onChange={e=>setDataBorrow({...dataBorrow, name:(e.target.value)})}/><br/>
+                 <input type="text"  placeholder="وصف العملية " className="border" onChange={e=>setDataBorrow({...dataBorrow, borrowDetails:(e.target.value)})}/><br/>
+                 </div>
+             </div>
+        </div>
 
-        <button  onClick={postOrder}>اضافه</button>
+        <div className='border'>
+            <div>
+            <label htmlFor="">سحب بطاقة الائتمان</label>
+                <div>
+                    <div className='flex flex-col'>
+                    <label htmlFor="">رقم البطاقة</label>
+                    <input type="text" className='border w-80' />
+                    </div>
+                    <div className='flex gap-5'>
+                        <div className='flex flex-col'>
+                            <label htmlFor="">تاريخ الإنتهاء</label>
+                            <input type="text" className='border w-48' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <label htmlFor="">CVV</label>
+                            <input type="text" className='border w-48' />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+
+        </div>
+
+
+
+        <button  onClick={postOrder}>سحب بطاقة الائتمان</button>
+    </div>
     </>
   )
 }
