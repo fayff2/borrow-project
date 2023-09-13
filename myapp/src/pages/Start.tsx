66b2027adf0078f8e446a38951f8431a0d297888
fayff2 <fay_ff2@hotmail.com>
2023-09-13 23:46:47 +0300

    edit

--- myapp/src/pages/Start.tsx
+++ myapp/src/pages/Start.tsx
@@ -7,7 +7,7 @@ export default function Start() {
    <NavbarSide/>
     
    <div className="flex justify-center items-center h-screen">
-    <div className="flex justify-center  mt-44 h-3/6  ">
+    <div className="flex justify-center  mt-10 h-3/6  ">
 <div className=" flex flex-col gap-10 lg:gap-16 max-w-sm lg:max-w-2xl p-10 bg-white  border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700  text-center items-center">
 
 <div className="">
